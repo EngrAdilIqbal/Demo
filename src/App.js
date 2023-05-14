92ad5ed83ad391aedb1486e27c9d23e2885cc662
EngrAdilIqbal <engradiliqbal989@gmail.com>
2023-05-14 15:34:06 +0500

    Demo

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import Feature from './Components/Feature';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+function App() {
+  return (
+    <div id="App">
+     <Navbar/>
+     <Header/>
+     <Feature/>
+     <About/>
+     <Contact/>
+    </div>
+  );
+}
+
+export default App;
